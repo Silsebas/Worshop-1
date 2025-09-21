33caf8a0f22745ef7209dc444d43984b3ac7ca5b
Silsebas <jordancubillo@gmail.com>
2025-09-21 11:56:27 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+// Establece la zona horaria (ajústala según la ubicación)
+date_default_timezone_set('America/Costa_Rica');
+
+// Muestra la fecha y hora actual
+echo date('d/m/Y H:i:s');
+?>
