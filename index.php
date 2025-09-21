b865714b29aee95e36c44601b920dfc71b47d6eb
Silsebas <jordancubillo@gmail.com>
2025-09-21 12:00:37 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <?php
-// Establece la zona horaria (ajústala según la ubicación)
+// Establece la zona horaria (ajustar según la ubicación)
 date_default_timezone_set('America/Costa_Rica');
 
 // Muestra la fecha y hora actual
 echo date('d/m/Y H:i:s');
 ?>
+
